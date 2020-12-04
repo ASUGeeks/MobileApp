9a26b4493e24ffc34b0f83e2a43b3c46cf3478ea
Mahmoud Hammad <hodaman2012@gmail.com>
2020-12-04 18:21:57 +0200

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View, } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>DREAm app is here</Text>
+      <Text>The DreAm app is here</Text>
       <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
