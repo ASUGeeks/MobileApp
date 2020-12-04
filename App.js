78fc4aa86fa0d242e212e8dbc35d41335e29dedf
Mahmoud Hammad <hodaman2012@gmail.com>
2020-12-04 23:27:37 +0200

    setup stack navidation

--- App.js
+++ App.js
@@ -1,22 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>The DreAm app is here</Text>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HOME" component={Home} />
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          // options={({ route }) => ({ title: route.params.name })}
+          options={({ route }) => ({ title: route.params.name })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fc3',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fc3",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
