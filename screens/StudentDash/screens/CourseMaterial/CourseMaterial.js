f3d146b9fc082363f84c01ff346010446dbbb67d
Mahmoud Hammad <hodaman2012@gmail.com>
2020-12-26 18:53:12 +0200

    create study material data structure

--- screens/StudentDash/screens/CourseMaterial/CourseMaterial.js
+++ screens/StudentDash/screens/CourseMaterial/CourseMaterial.js
@@ -2,6 +2,112 @@ import React, { useState, useEffect } from "react";
 import { View, StyleSheet, ScrollView, Text } from "react-native";
 import ContentList from "./components/ContentList";
 
+
+
+const content = [
+  {
+    title: "Week 1",
+    data: [
+      {
+        title: "recorded lectures",
+        data: [
+          {
+            title: "Lecture 1",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 2",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 3",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+        ],
+      },
+      {
+        title: "Written lectures",
+        data: [
+          {
+            title: "Lecture 1 pdf",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 2",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 3",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Week 1",
+    data: [
+      {
+        title: "recorded lectures",
+        data: [
+          {
+            title: "Lecture 4",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 5",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 6",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+        ],
+      },
+      {
+        title: "Written lectures",
+        data: [
+          {
+            title: "Lecture 4 pdf",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 5",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+          {
+            title: "Lecture 6",
+            description: "Ignore this description for now",
+            URL:
+              "https://drive.google.com/file/d/1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0/view",
+          },
+        ],
+      },
+    ],
+  },
+];
 export default ({ route }) => {
   const [Title, setTitle] = useState("");
   const { title, id } = route.params;
@@ -18,7 +124,7 @@ export default ({ route }) => {
 
   return (
     <ScrollView style={styles.root}>
-      <ContentList />
+      <ContentList content={content} />
     </ScrollView>
   );
 };
--- screens/StudentDash/screens/CourseMaterial/components/ContentList.js
+++ screens/StudentDash/screens/CourseMaterial/components/ContentList.js
@@ -1,69 +1,122 @@
-import * as React from "react";
+import React from "react";
+import { SectionList, FlatList, StyleSheet, Text, View } from "react-native";
 import { List } from "react-native-paper";
 
-const MyComponent = () => {
-  const [expanded, setExpanded] = React.useState(true);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: "bold",
+    backgroundColor: "rgba(247,247,247,1.0)",
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
 
-  const handlePress = () => setExpanded(!expanded);
-
+const SectionListBasics = () => {
   return (
     <List.Section title="Study material">
-      <List.Accordion
-        title="Week 1"
-        left={(props) => <List.Icon {...props} icon="folder" />}
-      >
-        <List.Accordion
-          title="Recorded lectures"
-          left={(props) => <List.Icon {...props} icon="folder" />}
-        >
-          <List.Item
-            title="Lecture 1"
-            left={(props) => <List.Icon {...props} icon="video" />}
-          ></List.Item>
-        </List.Accordion>
-        <List.Accordion
-          title="Written summary"
-          left={(props) => <List.Icon {...props} icon="folder" />}
-        >
-          <List.Item
-            title="Lecture 3"
-            left={(props) => <List.Icon {...props} icon="video" />}
-          />
-          <List.Item
-            title="Lecture 4"
-            left={(props) => <List.Icon {...props} icon="video" />}
-          />
-          <List.Item
-            title="Lecture 5"
-            left={(props) => <List.Icon {...props} icon="video" />}
-          />
-          <List.Item
-            title="Lecture 6"
-            left={(props) => <List.Icon {...props} icon="video" />}
-          />
-        </List.Accordion>
-      </List.Accordion>
-
-      <List.Accordion
-        title="Week 2 "
-        left={(props) => <List.Icon {...props} icon="folder" />}
-        expanded={expanded}
-        onPress={handlePress}
-      >
-        <List.Accordion
-          title="written lectures "
-          left={(props) => <List.Icon {...props} icon="folder" />}
-          expanded={expanded}
-          onPress={handlePress}
-        >
-          <List.Item
-            title="Lecture 1"
-            left={(props) => <List.Icon {...props} icon="file-pdf" />}
-          />
-        </List.Accordion>
-      </List.Accordion>
+      <FlatList
+        data={[
+          {
+            title: "Week1",
+            data: [
+              {
+                title: "Recorded lectures",
+                data: [{ title: "Lecture 1" }, { title: "Lecture 2" }],
+              },
+            ],
+          },
+        ]}
+        renderItem={({ item }) => (
+          <List.Accordion
+            title={item.title}
+            left={(props) => <List.Icon {...props} icon="folder" />}
+          >
+            <FlatList
+              data={item.data}
+              renderItem={({ item }) => (
+                <List.Accordion
+                  title={item.title}
+                  left={(props) => <List.Icon {...props} icon="folder" />}
+                >
+                  <FlatList
+                    data={item.data}
+                    renderItem={({ item }) => (
+                      <List.Item
+                        title={item.title}
+                        left={(props) => <List.Icon {...props} icon="video" />}
+                      ></List.Item>
+                    )}
+                  />
+                </List.Accordion>
+              )}
+            />
+          </List.Accordion>
+        )}
+      />
     </List.Section>
   );
 };
 
-export default MyComponent;
+export default SectionListBasics;
+
+// import * as React from "react";
+
+// const MyComponent = (content) => {
+//   const [expanded, setExpanded] = React.useState(true);
+
+//   const handlePress = () => setExpanded(!expanded);
+
+//   return (
+//     <List.Section title="Study material">
+//       <List.Accordion
+//         title="Week 1"
+//         left={(props) => <List.Icon {...props} icon="folder" />}
+//       >
+//         <List.Accordion
+//           title="Recorded lectures"
+//           left={(props) => <List.Icon {...props} icon="folder" />}
+//         >
+//           <List.Item
+//             title="Lecture 1"
+//             left={(props) => <List.Icon {...props} icon="video" />}
+//           ></List.Item>
+//         </List.Accordion>
+//         <List.Accordion
+//           title="Written summary"
+//           left={(props) => <List.Icon {...props} icon="folder" />}
+//         >
+//           <List.Item
+//             title="Lecture 3"
+//             left={(props) => <List.Icon {...props} icon="video" />}
+//           />
+//           <List.Item
+//             title="Lecture 4"
+//             left={(props) => <List.Icon {...props} icon="video" />}
+//           />
+//           <List.Item
+//             title="Lecture 5"
+//             left={(props) => <List.Icon {...props} icon="video" />}
+//           />
+//           <List.Item
+//             title="Lecture 6"
+//             left={(props) => <List.Icon {...props} icon="video" />}
+//           />
+//         </List.Accordion>
+//       </List.Accordion>
+//     </List.Section>
+//   );
+// };
+
+// export default MyComponent;
