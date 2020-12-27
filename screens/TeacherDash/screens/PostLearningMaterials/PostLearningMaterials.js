6d222e88d77cf38e618f7fc1617b980d8de9e4ff
Mahmoud Hammad <hodaman2012@gmail.com>
2020-12-27 20:16:45 +0200

    parse the study material as inputs

--- screens/TeacherDash/screens/PostLearningMaterials/PostLearningMaterials.js
+++ screens/TeacherDash/screens/PostLearningMaterials/PostLearningMaterials.js
@@ -1,26 +1,124 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, FlatList, Text, StyleSheet ,ScrollView} from "react-native";
 import Submit from "../../../../shared/Submit";
 import Input from "../../../../shared/Input";
+
 export default () => {
-  const [Title, setTitle] = React.useState("");
-  const [URL, setURL] = React.useState("");
+  const [Content, setContent] = useState([
+    {
+      title: "Week1",
+      data: [
+        {
+          title: "Recorded lectures",
+          type: "vid",
+          data: [
+            {
+              title: "Lecture 1",
+              URL: "OMMXrmMsjAk",
+            },
+            {
+              title: "Lecture 2",
+              URL: "OMMXrmMsjAk",
+            },
+          ],
+        },
+        {
+          title: "Written lectures",
+          type: "pdf",
+          data: [
+            {
+              title: "Lecture 1",
+              URL: "1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0",
+            },
+            {
+              title: "Lecture 2",
+              URL: "id=1qBs_Y1Yhc_lZhQ8ru3l3kSdmoo_6gBE0",
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+
+  const [Week, setWeek] = useState("");
+  const [URL, setURL] = useState("");
 
   function handleSubmit() {
     console.log("Submit");
   }
 
   return (
-    <View style={styles.root}>
-      <Input label="Title" value={Title} setValue={setTitle} />
-      <Input label="URL" value={URL} setValue={setURL} />
+    <ScrollView style={styles.root}>
+      <View style={[styles.week]}>
+        <FlatList
+          data={Content}
+          renderItem={(week) => (
+            <View style={[styles.week,styles.BL1]}>
+              <Input label="Title" value={week.item.title} setValue={setURL} />
+              {/* type start */}
+              <View style={styles.week}>
+                <FlatList
+                  data={week.item.data}
+                  renderItem={(type) => (
+                    <View style={[styles.week,styles.BL2]}>
+                      <Input
+                        label="Title"
+                        value={type.item.title}
+                        setValue={setURL}
+                      />
+                      <Input
+                        label="Type"
+                        value={type.item.type}
+                        setValue={setURL}
+                      />
+                      <View style={[styles.week,styles.BL3]}>
+                        <FlatList
+                          data={type.item.data}
+                          renderItem={(content) => (
+                            <View style={styles.week}>
+                              <Input
+                                label="Title"
+                                value={content.item.title}
+                                setValue={setURL}
+                              />
+                              <Input
+                                label="Url"
+                                value={content.item.URL}
+                                setValue={setURL}
+                              />
+                            </View>
+                          )}
+                        />
+                      </View>
+                    </View>
+                  )}
+                />
+              </View>
+              {/* type End */}
+            </View>
+          )}
+        />
+      </View>
       <Submit handleSubmit={handleSubmit} />
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   root: {
-    margin: 20,
+    marginVertical: 10,
   },
+  week: {
+    margin: 10,
+    borderLeftWidth:2,
+  },
+  BL1:{
+    borderLeftColor:"#ff0",
+  },
+  BL2:{
+    borderLeftColor:"#f00",
+  },
+  BL3:{
+    borderLeftColor:"#f0f",
+  }
 });
