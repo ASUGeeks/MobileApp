90ed5f9fde6c9806f5b4f7bd1eee492aac9f2583
= <hodaman2012@gmail.com>
2021-01-12 13:52:19 +0200

    create course

--- screens/Admin/screens/CreateCourse/CreateCourse.js
+++ screens/Admin/screens/CreateCourse/CreateCourse.js
@@ -3,6 +3,7 @@ import { StyleSheet, ScrollView } from "react-native";
 import Submit from "../../../../shared/Submit";
 import Input from "../../../../shared/Input";
 import Radio from "../../../../shared/Radio";
+import axios from "axios"
 
 export default () => {
   const [coursename, setcoursename] = useState("");
@@ -16,7 +17,26 @@ export default () => {
     // setspecifications("hello, this is the course specification");fbsszm.k;likil;ki8k.    ERE´FJFIF
   }, []);
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    
+    const course={
+      name:"computer organization",
+      code:"CSE2020",
+      profs:["simpo"],
+      TAs:[],
+      credit_hours:3,
+    }
+    const adminToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNWZmZDYzMjc5M2QyYWE1OWU5M2IwYTYzIiwidXNlcm5hbWUiOiJhZG1pbmFkbWluIiwiZW1haWwiOiJhZG1pbkBleGFtcGxlLmNvbSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYxMDQ0NDQwNX0.oQW_kkOz5CzJYPGnDjlUwozJzEIzP7BI7RR2qaI5R9E"
+    axios
+    .post("http://localhost:5100/create-course",course,{headers:
+    {token:adminToken}})
+    .then((r) => {
+        console.log("login",r.data)
+        // storeToken()
+    })
+    .catch((bug) => console.log("BUBUBUUB", bug))
+  }
+
 
   return (
     <ScrollView style={styles.root}>
